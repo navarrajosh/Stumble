5c3dc6514b64b8f4571cdbbf540096bbf8f153fb
migsbogs <75259766+migsbogs@users.noreply.github.com>
2021-12-02 17:44:15 +0800

    Navbar not yet fixed

--- app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Stumble.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Stumble.kt
@@ -2,10 +2,40 @@ package ph.edu.dlsu.mobdeve.navarra.joseph.stumble
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import ph.edu.dlsu.mobdeve.navarra.joseph.stumble.fragments.ChatFragment
+import ph.edu.dlsu.mobdeve.navarra.joseph.stumble.fragments.HomeFragment
+import ph.edu.dlsu.mobdeve.navarra.joseph.stumble.fragments.ProfileFragment
 
 class Stumble : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stumble)
+
+        val homeFragment = HomeFragment()
+        val chatFragment = ChatFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
+
+
+
+        botnav.setOnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.home -> makeCurrentFragment(homeFragment)
+                R.id.chat -> makeCurrentFragment(chatFragment)
+                R.id.profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
+
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.FLStumble, fragment)
+            commit()
+        }
 }
