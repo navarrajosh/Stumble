e25517a725239c3442ae74ffa18c4e4d8bfb2d54
migsbogs <75259766+migsbogs@users.noreply.github.com>
2022-01-04 19:46:51 +0800

    register done

--- app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Login.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Login.kt
@@ -1,11 +1,39 @@
 package ph.edu.dlsu.mobdeve.navarra.joseph.stumble
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+
+    var btn_submit : Button? = null
+    var et_username : EditText? = null
+    var et_password : EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_submit = findViewById(R.id.btn_submit)
+        et_username = findViewById(R.id.et_username)
+        et_password = findViewById(R.id.et_password)
+
+        var helper = stumbledb(applicationContext)
+        var db = helper.readableDatabase
+        var rs = db.rawQuery("SELECT * FROM USERS",null)
+
+        btn_submit!!.setOnClickListener{
+            var cv = ContentValues()
+            cv.put("UNAME", et_username?.text.toString())
+            cv.put("PWD",et_password?.text.toString())
+            db.insert("USERS",null,cv)
+
+            et_username?.setText("")
+            et_password?.setText("")
+            et_username?.requestFocus()
+        }
     }
 }
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/stumbledb.kt
@@ -0,0 +1,17 @@
+package ph.edu.dlsu.mobdeve.navarra.joseph.stumble
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class stumbledb(context:Context) : SQLiteOpenHelper(context, "USERDB",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE USERS(USERID INTEGER PRIMARY KEY AUTOINCREMENT, UNAME TEXT, PWD TEXT)")
+        db?.execSQL("INSERT INTO USERS(UNAME,PWD) VALUES ('user 1' , 'user123')")
+        db?.execSQL("INSERT INTO USERS(UNAME,PWD) VALUES ('user 2' , 'asdqwe123')")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
