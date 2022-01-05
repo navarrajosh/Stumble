e816e6208f2f670221c5d6fa669b77def217b0ea
navarrajosh <73941578+navarrajosh@users.noreply.github.com>
2022-01-05 17:21:44 +0800

    UPDATED REGISTER

--- app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/stumbledb.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/stumbledb.kt
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class stumbledb(context:Context) : SQLiteOpenHelper(context, "USERDB",null,1) {
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE USERS(USERID INTEGER PRIMARY KEY AUTOINCREMENT, UNAME TEXT, PWD TEXT)")
+        db?.execSQL("CREATE TABLE USERS(USERID INTEGER PRIMARY KEY AUTOINCREMENT, UNAME TEXT, PWD TEXT, EMAIL TEXT, fNAME TEXT, UNIV TEXT, COURSE TEXT, GENDER TEXT, AGE INT)")
         db?.execSQL("INSERT INTO USERS(UNAME,PWD) VALUES ('user 1' , 'user123')")
         db?.execSQL("INSERT INTO USERS(UNAME,PWD) VALUES ('user 2' , 'asdqwe123')")
     }
