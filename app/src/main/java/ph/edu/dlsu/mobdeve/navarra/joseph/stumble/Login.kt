87f0e319c0eb43caa2337f901114a1a5c27afc2a
migsbogs <75259766+migsbogs@users.noreply.github.com>
2022-01-05 18:36:35 +0800

    fixed login

--- app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Login.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Login.kt
@@ -1,10 +1,12 @@
 package ph.edu.dlsu.mobdeve.navarra.joseph.stumble
 
 import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class Login : AppCompatActivity() {
@@ -12,6 +14,8 @@ class Login : AppCompatActivity() {
     var btn_submit : Button? = null
     var et_username : EditText? = null
     var et_password : EditText? = null
+    var tv_signup : Button? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,20 +24,30 @@ class Login : AppCompatActivity() {
         btn_submit = findViewById(R.id.btn_submit)
         et_username = findViewById(R.id.et_username)
         et_password = findViewById(R.id.et_password)
+        tv_signup = findViewById(R.id.tv_signup)
 
         var helper = stumbledb(applicationContext)
         var db = helper.readableDatabase
-        var rs = db.rawQuery("SELECT * FROM USERS",null)
 
-        btn_submit!!.setOnClickListener{
-            var cv = ContentValues()
-            cv.put("UNAME", et_username?.text.toString())
-            cv.put("PWD",et_password?.text.toString())
-            db.insert("USERS",null,cv)
 
-            et_username?.setText("")
-            et_password?.setText("")
-            et_username?.requestFocus()
+        btn_submit!!.setOnClickListener {
+            var args =
+                listOf(et_username?.text.toString(), et_password?.text.toString()).toTypedArray()
+            var rs = db.rawQuery("SELECT * FROM USERS WHERE UNAME = ? AND PWD = ?", args)
+            if (rs.moveToNext()) {
+                Toast.makeText(applicationContext, "Login Successful",Toast.LENGTH_LONG).show()
+                val gotoStumble = Intent(applicationContext, Stumble::class.java)
+                startActivity(gotoStumble)
+
+            }else
+                Toast.makeText(applicationContext, "Invalid Credentials",Toast.LENGTH_LONG).show()
+        }
+
+
+
+        tv_signup!!.setOnClickListener{
+            val gotoRegister = Intent(applicationContext, Register::class.java)
+            startActivity(gotoRegister)
         }
     }
 }
--- app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Register.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Register.kt
@@ -1,10 +1,12 @@
 package ph.edu.dlsu.mobdeve.navarra.joseph.stumble
 
 import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class Register : AppCompatActivity() {
 
@@ -34,19 +36,19 @@ class Register : AppCompatActivity() {
 
         var helper = stumbledb(applicationContext)
         var db = helper.readableDatabase
-        var rs = db.rawQuery("SELECT * FROM USERS",null)
+        var rs = db.rawQuery("SELECT * FROM USERS", null)
 
-        btn_submit!!.setOnClickListener{
+        btn_submit!!.setOnClickListener {
             var cv = ContentValues()
             cv.put("UNAME", et_username?.text.toString())
-            cv.put("PWD",et_password?.text.toString())
+            cv.put("PWD", et_password?.text.toString())
             cv.put("EMAIL", et_email?.text.toString())
-            cv.put("fNAME",et_fullname?.text.toString())
+            cv.put("fNAME", et_fullname?.text.toString())
             cv.put("UNIV", et_school?.text.toString())
-            cv.put("COURSE",et_program?.text.toString())
-            cv.put("GENDER",et_gender?.text.toString())
-            cv.put("AGE",et_age?.text.toString())
-            db.insert("USERS",null,cv)
+            cv.put("COURSE", et_program?.text.toString())
+            cv.put("GENDER", et_gender?.text.toString())
+            cv.put("AGE", et_age?.text.toString())
+            db.insert("USERS", null, cv)
 
             et_username?.setText("")
             et_password?.setText("")
@@ -57,6 +59,14 @@ class Register : AppCompatActivity() {
             et_gender?.setText("")
             et_age?.setText("")
             et_username?.requestFocus()
+
+            val gotoLogin = Intent(applicationContext, Login::class.java)
+            startActivity(gotoLogin)
+
         }
+
     }
+
+
+
 }
