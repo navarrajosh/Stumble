f8e2ce7107a198a53d25bfc7bdb779d2bdc84ec1
navarrajosh <73941578+navarrajosh@users.noreply.github.com>
2022-02-12 16:13:12 +0800

    hehe

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/navarra/joseph/stumble/Message.kt
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.mobdeve.navarra.joseph.stumble
+
+class Message {
+    var message: String? = null
+    var senderId: String? = null
+
+    constructor(){}
+
+    constructor(message: String?, senderId: String?){
+        this.message = message
+        this.senderId = senderId
+    }
+
+}
